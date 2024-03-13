5952e5ea078bb649e8615674001d1be172a7635a
Alan Sartorio <alan42ga@hotmail.com>
2024-03-13 00:46:41 -0300

    Save plots in svg

--- Ejercicio/analyzer.py
+++ Ejercicio/analyzer.py
@@ -12,22 +12,52 @@ df.rename(columns={
 }, inplace=True)
 
 print(df.columns)
+figsize = (10, 10)
 
 # 1) elimino datos faltantes
-df.dropna()
+# df.dropna()
 
+def saveplot(filename):
+    def decorator(func):
+        def wrapper():
+            func()
+            plt.tight_layout()
+            plt.savefig(filename)
+            plt.show()
+            print(filename + " finished")
+        return wrapper
+    return decorator
+
+@saveplot("part_2.svg")
+def part_2():
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(ncols=2, nrows=2, figsize=figsize)
+    sns.boxplot(data=df, y='Grasas Saturadas', ax=ax1)
+    sns.boxplot(data=df, y='Alcohol', ax=ax2)
+    sns.boxplot(data=df, y='Calorías', ax=ax3)
+    sns.countplot(data=df, x='Sexo', ax=ax4)
+
+    ax4.bar_label(ax4.containers[0])
+
+
+
+part_2()
+
+@saveplot("part_3.svg")
 def part_3():
-    fig, (ax1, ax2, ax3) = plt.subplots(ncols=3)
+    fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=figsize)
     sns.boxplot(data=df, y='Grasas Saturadas', hue='Sexo', ax=ax1)
     sns.boxplot(data=df, y='Alcohol', hue='Sexo', ax=ax2)
     sns.boxplot(data=df, y='Calorías', hue='Sexo', ax=ax3)
-    plt.show()
+
 part_3()
 
+@saveplot("part_4.svg")
 def part_4():
+    plt.figure(figsize=figsize)
     df['Categoría'] = pd.cut(df['Calorías'], bins=[0, 1100, 1700, np.Infinity], include_lowest=True, labels=['0 - 1100', '1100 - 1700', '1700 - ∞'])
 
-    sns.scatterplot(data=df, x='Calorías', y='Alcohol', hue=df[['Categoría', 'Sexo']].apply(tuple, axis=1))
-    # sns.boxplot(data=df, x='Categoría', y='Alcohol', hue='Sexo')
-    plt.show()
+    # sns.scatterplot(data=df, x='Calorías', y='Alcohol', hue=df[['Categoría', 'Sexo']].apply(tuple, axis=1))
+    # sns.scatterplot(data=df, x='Calorías', y='Alcohol', hue='Categoría')
+    sns.boxplot(data=df, x='Categoría', y='Alcohol', hue='Sexo') #:like
+    
 part_4()
