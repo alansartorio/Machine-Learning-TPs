0e6261efa9109e55ec0bd6c72c4dca0b71863b00
Alan Sartorio <alan42ga@hotmail.com>
2024-04-08 23:12:13 -0300

    WIP: ej2
    Co-authored-by: Agustin-Galarza <Agustin-Galarza@users.noreply.github.com>

--- TP1/tp1.py
+++ TP1/tp1.py
@@ -1,9 +1,14 @@
 import pandas as pd
+from pandas import DataFrame
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 import json
+import re
+from typing import Callable
+import math
+import random
 show_plots = os.environ.get('SHOW_PLOTS', '1') == '1'
 
 sns.set_theme()
@@ -16,12 +21,15 @@ def laplace_correction(occurrences, total, classes_amount):
     return (occurrences + 1) / float(total + classes_amount)
     
 
-def calculate_probability_given(df: pd.DataFrame, var: str, value: str, given_var: str, given_value: str,):
+def calculate_predicate_probability_given(df: pd.DataFrame, predicate: Callable[[pd.Series], bool], given_var: str, given_value: str):
     in_class = df[df[given_var] == given_value]
     classes_amount = len(df[given_var].unique())
-    occurrences = (in_class[var] == value).sum()
+    occurrences = in_class.apply(predicate, 'columns').sum()
     total = len(in_class)
-    return  laplace_correction(occurrences, total, classes_amount) 
+    return laplace_correction(occurrences, total, classes_amount) 
+
+def calculate_probability_given(df: pd.DataFrame, var: str, value: str, given_var: str, given_value: str,):
+    return calculate_predicate_probability_given(df, lambda r:r[var] == value, given_var, given_value)
 
 def calculate_probability_of_being_in_class(var_probability: dict[str,dict[str,float]], class_probability:dict[str, float], values: dict[str, float], class_name: str):
     # P(clase/vars) = P(clase) * P(vars/clase) / P(vars) -> P(vars) is not necessary if the class is the result
@@ -69,6 +77,50 @@ def build_class_probability(df: pd.DataFrame, class_var: str, class_values: list
         class_probability[class_name] = (df[class_var] == class_name).sum() / len(df)
     return class_probability
 
+def split_training_and_evaluation(df: DataFrame, training_frac: float) -> tuple[DataFrame, DataFrame]:
+    training, evaluation = DataFrame(), DataFrame()
+    for clazz in df['categoria'].unique():
+        class_subset = df[df['categoria'] == clazz]
+        training_mask =  np.random.rand(len(class_subset)) < training_frac
+        training = pd.concat((training, class_subset[training_mask]))
+        evaluation = pd.concat((evaluation, class_subset[~training_mask]))
+    return (training, evaluation)
+
+def get_cofusion_matrix(df: DataFrame):
+    pass
+
+def pre_process_data(df: DataFrame) -> DataFrame:
+    def sanitize_title(s: str):
+        # Remove symbols
+        s = re.sub(r'[^\w]', ' ', s).lower()
+        # Remove stop words
+        # s = s
+        # Tokenize string
+        return [v for v in s.split() if v]
+    df['data'] = df['titular'].apply(sanitize_title)
+
+
+def get_vocabulary(df: DataFrame):
+    vocabulary = set()
+    for vector in df['data']:
+        vocabulary.update(vector)
+    return vocabulary
+
+
+# 3. Contar la cantidad de documentos de una clase que contienen a la palabra / la cantidad de documentos de esa clase
+def get_word_probability(word: str, clazz: str, df: DataFrame):
+    return calculate_predicate_probability_given(df, lambda r:word in r['data'], 'categoria', clazz)
+
+def classify_news(df: DataFrame, vocabulary: set) -> list[int]:
+    class_name = 'categoria'
+    classes = set(df[class_name].tolist())
+    var_probability = build_var_probability(df, vocabulary, class_name, )
+
+    results = []
+    for document in df['data']:
+        pass
+        # res = classify()
+
 def part_1(df):
     classes = ('I', 'E')
     variables = ('scones','cerveza','wiskey','avena','futbol')
@@ -85,6 +137,14 @@ def part_1(df):
     x2={'scones': 0, 'cerveza':1, 'wiskey': 1, 'avena':0, 'futbol':1}
     print("b)", x2, classify(var_probability, class_probability, x2))
 
+def part_2(df):
+    pre_process_data(df)
+    df.dropna(inplace=True)
+    # vocabulary = get_vocabulary(df)
+    training, evaluation = split_training_and_evaluation(df, 0.8)
+    print(len(training), len(evaluation))
+    # print(get_word_probability("el", "Deportes", df))
+    # print(len(vocabulary))
 
 def part_3(df):
     def clean_data(df):
@@ -109,10 +169,11 @@ def part_3(df):
     print("b)", x2, classify(var_probability, class_probability, x2))
 
 
-    
 
-print("Parte 1")
-part_1(preferencias_britanicos)
-# print("Parte 2")
+
+# print("Parte 1")
+# part_1(preferencias_britanicos)
+print("Parte 2")
+part_2(noticias_argentinas)
 # print("Parte 3")
 # part_3(binary)
