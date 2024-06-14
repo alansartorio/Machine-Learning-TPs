ce0b708d2a35af293d3757cc8da6d72b7e36518e
Alan Sartorio <alan42ga@hotmail.com>
2024-06-14 12:09:15 -0300

    Add k_means plots

---
+++ TP4/src/k_means_plots.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import polars as pl
+
+sns.set_theme()
+
+df = pl.read_csv('out/iterations.csv')
+
+df = df.group_by(['run', 'k']).last()
+print(df)
+
+sns.lineplot(data = df, x = 'k', y = df['error'] / df['k'])
+plt.show()
+
+# print(df)
