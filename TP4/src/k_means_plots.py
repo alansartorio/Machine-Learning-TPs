aae544a6c3c091d2df85a9daa1ecc93e59c9302a
Alan Sartorio <alan42ga@hotmail.com>
2024-06-14 19:32:24 -0300

    Fix name in k_means plot

--- TP4/src/k_means_plots.py
+++ TP4/src/k_means_plots.py
@@ -4,7 +4,8 @@ import polars as pl
 
 sns.set_theme()
 
-df = pl.read_csv('out/k_means_all_columns.csv')
+# df = pl.read_csv('out/k_means_iterations_all_columns.csv')
+df = pl.read_csv('out/k_means_iterations_numeric_columns.csv')
 
 df = df.group_by(['run', 'k']).last()
 print(df)
